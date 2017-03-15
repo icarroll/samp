d37db870441f131a8dfea76ecdcb7ab902ec923b
Isaac Carroll <icarroll@pobox.com>
2017-03-14 22:09:15 -0700

    on GET read from db and send list of tuples

--- samp.py
+++ samp.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
 
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import sqlite3
 
 class SampHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         if self.path != "/post":
-            self.send_error(404, "No such endpoint")
+            self.send_error(404)
 
         print("doing POST", self.requestline, self.command, self.path)
 
     def do_GET(self):
         if self.path != "/posts":
-            self.send_error(404, "No such endpoint")
+            self.send_error(404)
 
-        print("doing GET", self.requestline, self.command, self.path)
+        c = sqlite3.connect("blog.db").cursor()
+        c.execute("SELECT post_id,title,body FROM posts;")
+        posts = c.fetchall()
+        c.close()
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(str(posts).encode())
 
 def run():
     server_address = ("", 8000)
